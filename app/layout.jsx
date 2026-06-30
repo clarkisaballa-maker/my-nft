40802ba2514dfbdb23720c8fa4d930f440c2c1c5
clarkisaballa-maker <Clarkisaballa@gmail.com>
2026-06-30 09:44:46 +0500

    update 1

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,12 @@
-import { LiveSupportProvider } from './AllContext/ChatContext';
-import { UsersProvider } from './AllContext/UsersContext';
-import './globals.css'
+import Script from "next/script";
+import { LiveSupportProvider } from "./AllContext/ChatContext";
+import { UsersProvider } from "./AllContext/UsersContext";
+import "./globals.css";
+
+export const metadata = {
+  title: "Movee Tech",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -8,15 +14,49 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Movee Tech</title>
+
+        {/* Facebook Meta Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)n=f.fbq;
+            n.push=n.queue=[];
+            n.loaded=!0;
+            n.version='2.0';
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '2214789792589157');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
+
       <body>
-        <UsersProvider><LiveSupportProvider>{children}</LiveSupportProvider></UsersProvider>
+        {/* Facebook Pixel NoScript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2214789792589157&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
+        <UsersProvider>
+          <LiveSupportProvider>
+            {children}
+          </LiveSupportProvider>
+        </UsersProvider>
       </body>
     </html>
-  )
+  );
 }
-
-export const metadata = {
-  generator: 'v0.dev'
-};
